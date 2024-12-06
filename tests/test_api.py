7e3a59ec214a9adea0034d6a266c7dd5e060908f
Mateo Di Loreto <mateoadiloreto@gmail.com>
2024-12-06 13:49:33 -0300

    Fix http errors from 500 to 400

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,14 +1,14 @@
 import os
 import requests
 
-ENDPOINT = os.getenv("TEST_ENDPOINT", "http://localhost:5000")
+ENDPOINT = os.getenv("TEST_ENDPOINT", "http://localhost:4000")
 AZURE_ENDPOINT = os.getenv("TEST_AZURE_ENDPOINT")
 AZURE_CREDENTIALS = os.getenv("TEST_AZURE_CREDENTIALS")
 URL = os.getenv("TEST_URL", "https://madsblog.net/2024/10/29/kubernetes-networking-parte-2/")
 
 def test_translate_missing_body():
     response = requests.post(f"{ENDPOINT}/api/translate", json=None)
-    assert response.status_code == 500, f"Expected 500, but got {response.status_code}"
+    assert response.status_code == 400, f"Expected 400, but got {response.status_code}"
 
 def test_translate_valid_translator():
     payload = {
